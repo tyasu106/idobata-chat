e8620cef7ec2638957fbe94225924081bb7bb3fc
tyasu106 <t.yasu106@gmail.com>
2021-04-14 15:57:01 +0900

    fetch messages

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import { messagesRef } from "../firebase";
 
 const useStyles = makeStyles({
   root: {
@@ -7,9 +8,33 @@ const useStyles = makeStyles({
   },
 });
 
+// firebaseから取得したデータ
+// key -MYCUlOTdhGKoAloXY6B: value {name: "toshi", text: "ffff"}
+//      ↓↓↓変更
+// 下記を要素とする配列を作っていく
+// {key -MYCUlOTdhGKoAloXY6B, value name: "toshi", text: "ffff"}
+
 const MessageList = () => {
+  const [messages, setMessages] = useState([]);
   const classes = useStyles();
-  return <div className={classes.root} >MessageList</div>
-}
+
+  useEffect(() => {
+    messagesRef
+      .orderByKey()
+      .limitToLast(3)
+      .on("value", (snapshot) => {
+        const messages = snapshot.val();
+        if (messages === null) return;
+        const entries = Object.entries(messages);
+        const newMessages = entries.map((entry) => {
+          const [key, nameAndText] = entry;
+          // ... ← 展開する
+          return { key, ...nameAndText };
+        });
+        setMessages(newMessages);
+      });
+  }, []);
+  return <div className={classes.root}>MessageList</div>;
+};
 
 export default MessageList;
--- src/firebase.js
+++ src/firebase.js
@@ -20,8 +20,9 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
-const messagesRef = database.ref("messages");
+export const messagesRef = database.ref("messages");
 
 export const pushMessage = ({ name, text }) => {
   messagesRef.push({ name, text });
 };
+
